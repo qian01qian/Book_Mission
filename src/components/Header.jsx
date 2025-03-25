6b21ae72c3b8a0b8d2facfb044cb61dd8ec42654
qian01qian <y143m.ray@gmail.com>
2025-03-25 14:20:03 +0800

    fix_logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router";
-import Logo from "../public/logo.png";
 
 function Header({ title, slogan }) {
     return (
         <header className="min-h-20 flex flex-col items-center justify-center bg-gradient-to-b from-blue-200 to-white text-black">
             <Link to="/">
-                <img className="w-40 h-40 bg-center" src={Logo} alt="logo" />
+                <img className="w-40 h-40 bg-center" src="Logo.png" alt="logo" />
             </Link>
             <h2 className="text-black text-5xl font-bold text-center">
                 {title}
