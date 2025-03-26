a7288af509457f6a8d226b68bb0db9e05f7e9555
qian01qian <y143m.ray@gmail.com>
2025-03-26 11:37:58 +0800

    Pics_Route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header({ title, slogan }) {
     return (
         <header className="min-h-20 flex flex-col items-center justify-center bg-gradient-to-b from-blue-200 to-white text-black">
             <Link to="/">
-                <img className="w-40 h-40 bg-center" src="Logo.png" alt="logo" />
+                <img className="w-40 h-40 bg-center" src="/Logo.png" alt="logo" />
             </Link>
             <h2 className="text-black text-5xl font-bold text-center">
                 {title}
